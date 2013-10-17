4f2b704409f6480352f7e0f22321c8f138e18288
Bluepsophage <bluepsophage@supinternet.fr>
2013-10-17 16:47:20 +0200

    Dossiers classes et interfaces + base

---
+++ models/classes/Personnage.php
@@ -0,0 +1,14 @@
+<?php
+
+abstract class Character {
+    protected $name;
+    protected $type;
+    protected $inventory = null;
+    protected $strenght;
+    protected $health;
+    protected $intelligence;
+
+
+}
+
+?>
