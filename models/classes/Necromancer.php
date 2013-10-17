257bd69b2d98f34a993eab531146fe7ea7759594
Nirnae <nicofoub@hotmail.fr>
2013-10-17 18:39:35 +0200

    add ClassWarrior and Necromancer

---
+++ models/classes/Necromancer.php
@@ -0,0 +1,21 @@
+<?php 
+
+class Necromancer extends Character implements NecromancerInterface{
+	CONST type = 'Necromancer';
+
+	public function __construct(){
+		$this->health += 150;
+	}
+	
+	public function getType(){
+		return self::type;
+	}
+
+	//
+	public function lifeSteal(Character $enemy);
+
+	//
+	public function strengthOfLife(Character $enemy);
+}
+
+?>
---
+++ models/classes/Warrior.php
@@ -0,0 +1,22 @@
+<?php
+
+class Warrior extends Character implements WarriorInterface{
+	CONST type = 'Warrior';
+
+	public function __construct(){
+		$this->strength += 30;
+		//parent::__construct();
+	}
+
+	public function getType(){
+		return self::type;
+	}
+
+	//Attaque plus puissante (200)
+	public function powerfulHit(Character $enemy);
+
+	//Evite l'attaque
+	public function dodge();
+
+}
+?>
