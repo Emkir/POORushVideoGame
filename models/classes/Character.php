00fef60cbadda328205ff28618b3196ab88db6c3
Emmeric Royer <emmeric.royer@supinternet.fr>
2013-10-17 18:02:10 +0200

    character class

--- models/classes/Character.php
+++ models/classes/Character.php
@@ -1,14 +1,52 @@
 <?php
 
-abstract class Character {
+abstract class Character implements CharacterInterface{
     protected $name;
-    protected $type;
-    protected $inventory = null;
-    protected $strenght;
-    protected $health;
-    protected $intelligence;
+    protected $inventory = array();
+    protected $strength = 100;
+    protected $health = 500;
+    protected $intelligence = 100;
 
 
+    public function setName($name){
+        $this->name = $name;
+    }
+    public function setStrength($strength){
+        $this->strength = $strength;
+    }
+    public function setIntelligence($intelligence){
+        $this->intelligence = $intelligence;
+    }
+    public function setHealth($hp){
+        $this->health = $hp;
+    }
+
+    /*
+    *	GETTER
+    */
+    public function getName(){
+        return $this->name;
+    }
+    public function getStrength(){
+        return $this->strength;
+    }
+    public function getIntelligence(){
+        return $this->intelligence;
+    }
+    public function getHealth(){
+        return $this->health;
+    }
+
+    //
+    public function attack(Player $enemy){
+
+    }
+    public function heal(){
+
+    }
+    public function defend(){
+
+    }
 }
 
 ?>
--- models/interfaces/CharacterInterface.php
+++ models/interfaces/CharacterInterface.php
@@ -1,3 +1,27 @@
 <?php
 
+interface CharacterInterface
+{
+    /*
+    *	SETTER
+    */
+    public function setName($name);
+    public function setStrength($strengh);
+    public function setIntelligence($intelligence);
+    public function setHealth($hp);
+
+    /*
+    *	GETTER
+    */
+    public function getName();
+    public function getStrength();
+    public function getIntelligence();
+    public function getHealth();
+
+    //
+    public function attack(Player $enemy);
+    public function heal();
+    public function defend();
+}
+
 ?>
