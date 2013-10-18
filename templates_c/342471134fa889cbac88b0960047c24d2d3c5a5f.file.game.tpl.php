8279e333f5bd8f0c27383cba92620667cac693af
Anthon95 <anthony.meyer.95@gmail.com>
2013-10-18 16:21:37 +0200

    control

--- templates_c/342471134fa889cbac88b0960047c24d2d3c5a5f.file.game.tpl.php
+++ templates_c/342471134fa889cbac88b0960047c24d2d3c5a5f.file.game.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.13, created on 2013-10-18 13:03:23
+<?php /* Smarty version Smarty-3.1.13, created on 2013-10-18 14:17:43
          compiled from "templates\game.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:3082352610abaa0c756-45194854%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '342471134fa889cbac88b0960047c24d2d3c5a5f' => 
     array (
       0 => 'templates\\game.tpl',
-      1 => 1382101402,
+      1 => 1382105818,
       2 => 'file',
     ),
   ),
@@ -24,11 +24,13 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'health_p1' => 0,
     'strength_p1' => 0,
     'intel_p1' => 0,
+    'player1' => 0,
     'perso2' => 0,
     'nickname2' => 0,
     'health_p2' => 0,
     'strength_p2' => 0,
     'intel_p2' => 0,
+    'player2' => 0,
   ),
   'has_nocache_code' => false,
 ),false); /*/%%SmartyHeaderCode%%*/?>
@@ -44,14 +46,17 @@ $_valid = $_smarty_tpl->decodeProperties(array (
 <br>
 	Intelligence : <?php echo $_smarty_tpl->tpl_vars['intel_p1']->value;?>
 <br> 
-	<input type="button" value="Attaquer" name="attack1">
-	<input type="button" value="Se soigner" name="heal1">
-	<input type="button" value="Se prot&eacute;ger" name="protect1">
-	<input type="button" value="Sp&eacute;cial 1" name="special1_1">
-	<input type="button" value="Sp&eacute;cial 2" name="special1_2"><br/>
-	
- </table>
-
+	<a href="index.php?action=gameAction&amp;attacker=<?php echo $_smarty_tpl->tpl_vars['player1']->value;?>
+&amp;gameAction=attack"><button>Attaquer</button></a>
+	<a href="index.php?action=gameAction&amp;player1=<?php echo $_smarty_tpl->tpl_vars['player1']->value;?>
+&amp;gameAction=heal"><button>Se soigner</button></a>
+	<a href="index.php?action=gameAction&amp;player1=<?php echo $_smarty_tpl->tpl_vars['player1']->value;?>
+&amp;gameAction=protect"><button>Se prot&eacute;ger</button></a>
+	<a href="index.php?action=gameAction&amp;player1=<?php echo $_smarty_tpl->tpl_vars['player1']->value;?>
+&amp;gameAction=special1"><button>Sp&eacute;cial 1</button></a>
+	<a href="index.php?action=gameAction&amp;player1=<?php echo $_smarty_tpl->tpl_vars['player1']->value;?>
+&amp;gameAction=special2"><button>Sp&eacute;cial 2</button></a>
+</table>
 	<br><br>
 
  <table>
@@ -65,11 +70,16 @@ $_valid = $_smarty_tpl->decodeProperties(array (
 <br>
 	Intelligence : <?php echo $_smarty_tpl->tpl_vars['intel_p2']->value;?>
 <br> 
-	<input type="button" value="Attaquer" name="attack2">
-	<input type="button" value="Se soigner" name="heal2">
-	<input type="button" value="Se prot&eacute;ger" name="protect2">
-	<input type="button" value="Sp&eacute;cial 1" name="special2_1">
-	<input type="button" value="Sp&eacute;cial 2" name="special2_2">
+	<a href="index.php?action=gameAction&amp;attacker=<?php echo $_smarty_tpl->tpl_vars['player2']->value;?>
+&amp;gameAction=attack"><button>Attaquer</button></a>
+	<a href="index.php?action=gameAction&amp;player2=<?php echo $_smarty_tpl->tpl_vars['player2']->value;?>
+&amp;gameAction=heal"><button>Se soigner</button></a>
+	<a href="index.php?action=gameAction&amp;player2=<?php echo $_smarty_tpl->tpl_vars['player2']->value;?>
+&amp;gameAction=protect"><button>Se prot&eacute;ger</button></a>
+	<a href="index.php?action=gameAction&amp;player2=<?php echo $_smarty_tpl->tpl_vars['player2']->value;?>
+&amp;gameAction=special1"><button>Sp&eacute;cial 1</button></a>
+	<a href="index.php?action=gameAction&amp;player2=<?php echo $_smarty_tpl->tpl_vars['player2']->value;?>
+&amp;gameAction=special2"><button>Sp&eacute;cial 2</button></a>
  </table>
 
  <?php if ($_smarty_tpl->tpl_vars['health_p1']->value==0||$_smarty_tpl->tpl_vars['health_p2']->value==0){?>
